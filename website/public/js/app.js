8cfaa34f53a93c9986e976124ca6d489886a3c26
Vikrum Nijjar <vikrum@google.com>
2017-09-22 16:44:57 -0700

    Change thumbnail to alternate.

--- website/public/js/app.js
+++ website/public/js/app.js
@@ -1,8 +1,10 @@
 var db = firebase.database();
 
+// show.preview.medium
+// show.thumbnails.medium[1].url
 Vue.component('snl-item', {
   props: ['show'],
-  template: '<div class="snlitem"><p>{{ show.title }}</p><a :href="show.url"><img :src="show.preview.medium" class="img-responsive" style="max-width:100%" :alt="show.title" :title="show.title"></a></div>'
+  template: '<div class="snlitem"><p>{{ show.title }}</p><a :href="show.url"><img :src="show.thumbnails.medium[1].url" class="img-responsive" style="max-width:100%" :alt="show.title" :title="show.title"></a></div>'
 })
 
 var app7 = new Vue({
