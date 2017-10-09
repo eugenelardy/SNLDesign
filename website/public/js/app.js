4ce512485f1cc20e8fd2a6e0a3b676a21ba052ae
Vikrum Nijjar <vsnijjar@gmail.com>
2017-10-08 22:34:36 -0700

    Limit to 12 episodes.

--- website/public/js/app.js
+++ website/public/js/app.js
@@ -4,12 +4,15 @@ var db = firebase.database();
 // show.thumbnails.medium[1].url
 Vue.component('snl-item', {
   props: ['show'],
-  template: '<div class="snlitem"><p>{{ show.title }}</p><a :href="show.url"><img :src="show.thumbnails.medium[1].url" class="img-responsive" style="max-width:100%" :alt="show.title" :title="show.title"></a></div>'
+  methods: {
+    secondsToHms: function(d) { d = Number(d); var h = Math.floor(d / 3600); var m = Math.floor(d % 3600 / 60); var s = Math.floor(d % 3600 % 60); return ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2); }
+  },
+  template: '<div class="snlitem"><p>{{ show.title }}</p><div class="snlthumb"><div class="snlthumbtag">&#x1F552; &nbsp; {{ this.secondsToHms(show.length) }}</div><a :href="show.url"><img :src="show.thumbnails.medium[1].url" class="img-responsive" style="max-width:100%" :alt="show.title" :title="show.title"></a></div></div>'
 })
 
-var app7 = new Vue({
+var app = new Vue({
   el: '#snlweouthere',
   firebase: {
-    showlist: db.ref('videos/')
-  }
+    showlist: db.ref('videos/').limitToFirst(12)
+  },
 })
