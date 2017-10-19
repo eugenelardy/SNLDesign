8bad13aff2e1f04f7798367a9a3b8509f4dcfea5
Vikrum Nijjar <vsnijjar@gmail.com>
2017-10-18 23:21:42 -0700

    Updates presentation.

--- intarwebs/src/components/CasualItem.vue
+++ intarwebs/src/components/CasualItem.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="casualitem">
-  <p class="casualtitle">{{ show.snippet.title }}</p>
+  <p class="vtitle">{{ show.snippet.title }}</p>
   <div class="snlthumb">
-    <a :href="show.id.videoId">
+    <a :href="'https://www.youtube.com/watch?v=' + show.id.videoId">
       <img :src="show.snippet.thumbnails.medium.url" class="img-responsive" style="max-width:100%" :alt="show.snippet.title" :title="show.title">
     </a>
   </div>
@@ -20,13 +20,10 @@ export default {
 <style scoped>
 .casualitem {
   padding: 1em;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
   max-width: 350px;
 }
 
-.casualtitle {
+.vtitle {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
--- intarwebs/src/components/SNLItem.vue
+++ intarwebs/src/components/SNLItem.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="snlitem">
-  <p>{{ show.title }}</p>
+  <p class="vtitle">{{ show.title }}</p>
   <div class="snlthumb">
     <a :href="show.url">
       <img :src="show.thumbnails.medium[1].url" class="img-responsive" style="max-width:100%" :alt="show.title" :title="show.title">
@@ -20,5 +20,11 @@ export default {
 <style scoped>
 .snlitem {
   padding: 1em;
+}
+
+.vtitle {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
